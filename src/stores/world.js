786d1f01492b2a1bc774ecb81d870ff45d825527
Andrew Wooldridge <triptych@gmail.com>
2020-09-07 11:18:35 -0700

    Making the world a store, and adding random events in the store so they show up

---
+++ src/stores/world.js
@@ -0,0 +1,93 @@
+import { writable } from "svelte/store";
+export const world = writable([
+  {
+    id: 0,
+    description: `Upper Left`,
+    trigger: {
+      x: 0,
+      y: 0,
+    },
+    weight: 100,
+  },
+  {
+    id: 45,
+    description: `It's raining here`,
+    trigger: {
+      x: 0,
+      y: 0,
+    },
+    weight: 100,
+  },
+  {
+    id: 1,
+    description: "Upper Middle",
+    trigger: {
+      x: 1,
+      y: 0,
+    },
+    weight: 100,
+  },
+  {
+    id: 2,
+    description: "Upper Right",
+    trigger: {
+      x: 2,
+      y: 0,
+    },
+    weight: 100,
+  },
+  {
+    id: 3,
+    description: "Middle Right",
+    trigger: {
+      x: 2,
+      y: 1,
+    },
+    weight: 100,
+  },
+  {
+    id: 0,
+    description: `This is the starting point. See it there on the ground.`,
+    trigger: {
+      x: 1,
+      y: 1,
+    },
+    weight: 100,
+  },
+  {
+    id: 0,
+    description: `Middle left`,
+    trigger: {
+      x: 0,
+      y: 1,
+    },
+    weight: 100,
+  },
+  {
+    id: 2,
+    description: "Bottom Left",
+    trigger: {
+      x: 0,
+      y: 2,
+    },
+    weight: 100,
+  },
+  {
+    id: 2,
+    description: "Bottom Middle",
+    trigger: {
+      x: 1,
+      y: 2,
+    },
+    weight: 100,
+  },
+  {
+    id: 2,
+    description: "Bottom Right",
+    trigger: {
+      x: 2,
+      y: 2,
+    },
+    weight: 100,
+  },
+]);
